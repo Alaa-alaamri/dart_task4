658f3379de5326672fe72f74e8532ddd765904f6
Alaa Al-Aamria <138430263+Alaa-alaamri@users.noreply.github.com>
2023-08-24 09:37:13 +0400

    Initial commit

---
+++ bin/dart_task4.dart
@@ -0,0 +1,21 @@
+
+void main() {
+
+Map newmap = {
+  mapKyes.name.name:"Alaa",
+  mapKyes.email.name: "alaa@gmail.com",
+  mapKyes.address.name:"muscat",
+  mapKyes.phone.name:"9827283"
+};
+
+print(newmap);
+
+
+}
+
+enum mapKyes {
+  name,
+  email,
+  phone,
+  address
+}
